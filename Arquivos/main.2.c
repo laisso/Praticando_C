d17ff933e990310da087324562d7fc6a6466e73b
laisso <109741712+laisso@users.noreply.github.com>
2023-01-06 16:26:26 -0300

    Scanf

---
+++ Arquivos/main.2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int numero;
+    printf ("digite um numero: ");
+    scanf ("%d", &numero);
+    printf ("numero digitado: %d", numero);
+    return 0;
+}
