2523ccfe336eb3298014049e3b876ff0af3d51c4
laisso <109741712+laisso@users.noreply.github.com>
2023-01-06 16:25:20 -0300

    Define

--- Arquivos/main.c
+++ Arquivos/main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#define PI 3.1415
 int main()
 {
-    int numero;
-    printf ("digite um numero: ");
-    scanf ("%d", &numero);
-    printf ("numero digitado: %d", numero);
+    float f = PI+1;
+    printf("PI = %f \n", f);
+    /*PI = 12.4; essa definição jamais pode acontecer neste código porque uma vez que eu defini que PI
+    é 3.1415 com o comando define, não tem como ser alterado.*/
+
     return 0;
 }
