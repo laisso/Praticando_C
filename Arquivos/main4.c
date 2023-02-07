2aa48e9a9728816323b4963f95fc1f11a0de6ec6
laisso <109741712+laisso@users.noreply.github.com>
2023-02-07 15:10:54 -0300

    Caractere de escape \n

---
+++ Arquivos/main4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+// Caractere de escape para saltar uma linha -> \n
+int main()
+{
+    printf("\n-----------------------------------\n");
+    printf ("\n1- Logar  2- Cadastrar 3- Imprimir\n");
+    printf("\n-----------------------------------\n");
+// printf é uma função de saída(Output)
+    printf("\n Retorna um valor: %d" , printf("\n Oi!"));
+    return 0;
+}
