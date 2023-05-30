8127e4e2fdf0d883a68daff7ac26aea0de363f0b
laisso <109741712+laisso@users.noreply.github.com>
2023-05-30 13:58:02 -0300

    Vários valores com scanf

---
+++ Arquivos/main11.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// lendo vários valores
+
+int main()
+
+{
+    int num1, num2, num3 ;
+    printf("Digite tres valores inteiros: ");
+    scanf("%d %d %d",&num1,&num2,&num3);
+    printf("Os tres valores escolhidos:%d,%d e %d", num1, num2,num3);
+    return 0;
+}
