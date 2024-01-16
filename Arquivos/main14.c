22c352cc1428de7e89dc77503a42373d5c29075f
Laís Silva <109741712+laisso@users.noreply.github.com>
2024-01-16 10:30:55 -0300

    Update main14.c

--- Arquivos/main14.c
+++ Arquivos/main14.c
@@ -16,6 +16,10 @@ int main()
 
     printf("primeira letra: %c \nsegunda letra: %c\n", a, b);
 
+   /* testando buffer(espaço) do teclado => "%c%c" precisa de espaço entre eles para funcionar, porque meu programa vai entender 
+    o buffer como um caracter, portanto fica "%c %c" */
+
+
 
     return 0;
 }
