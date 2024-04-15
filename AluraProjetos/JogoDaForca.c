da7046cdcf59cd03a02fa5e533e9e45c5c8c630f
Laís Silva <109741712+laisso@users.noreply.github.com>
2024-04-15 20:48:18 -0300

    Create JogoDaForca.c

---
+++ AluraProjetos/JogoDaForca.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main(){
+
+char PalavraSecreta[20];
+
+sprintf(PalavraSecreta, "MELANCIA"); // sprintf recebe MELANCIA
+
+int acertou = 0;
+int enforcou = 0;
+
+
+do{
+// comecar o nosso jogo!!
+
+char chute;
+scanf("%c", &chute);
+
+for(int i = 0; i < strlen(PalavraSecreta); i++){
+    if(PalavraSecreta[i]== chute){
+        printf("A posicao %d tem essa letra!\n", i);
+    }
+}
+
+}while(!acertou && !enforcou);
+
+
+
+
+return 0;
+
+}
